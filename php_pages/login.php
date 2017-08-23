c6dce5c6d8f50c0533c8b1bdeade0d2988d1a1ed
jorbydeck <jordanwifwafu@yahoo.com>
2017-08-23 11:08:55 +0800

    log update

--- php_pages/login.php
+++ php_pages/login.php
@@ -1,74 +1,26 @@
 <?php
 ob_start();
 ini_set('memory_limit', '1024M'); // or you could use 1G
-include("../_func/_connmysql.php");
-include("../_func/_checkFields.php");
-session_start();
 
-// $logusername = isset($_POST['uname']);
-// $logpass = isset($_POST['pass']);
-
-    $err = $errAllfields = "";
-
-if($_SERVER['REQUEST_METHOD'] == 'POST') {
-    if(empty($_POST['uname']) || empty($_POST['pass'])) {
-
-        $err = '*Please fill all the fields';
-
-    }else {
-
-        $myusername = mysqli_real_escape_string($db,$_POST['uname']);
-        $mypassword = mysqli_real_escape_string($db,$_POST['pass']); 
-
-        checkFields::_clean($myusername);
-        checkFields::_clean($mypassword);
-       
-
-              
-        $sql = "SELECT uid FROM users WHERE username = '$myusername' and password = '$mypassword'";
-        $result = mysqli_query($db,$sql);
-        $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
-        $active = $row['active'];
-              
-        $count = mysqli_num_rows($result);
-          
-        // If result matched $myusername and $mypassword, table row must be 1 row
-                
-            if($count == 1) {
-               
-                $myusername = $_SESSION['username'];
-                 
-                header("Location: index.php");
-            }else {
-                $err = "*Your Login Name or Password is invalid";
-            }
-
-    }
-}
-   
-
-/*
-
-else if(isset($_POST['register'])){
-    $regusername = mysqli_real_escape_string($db, $_POST['myuname']);
-    $regpass = mysqli_real_escape_string($db, $_POST['mypass']);
-}
-
- */ 
+require 'action.php';
+//include("../_func/_logFile.php");
+global $authCode;
+$authCode = 1768; //global variable for authentication code
+//******METHODS******//
+//******************//
+                      
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
-
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>SB Admin 2 - Bootstrap Admin Theme</title>
+    <title>POSO TRAFFICE VIOLATION MANAGEMENT SYSTEM V1.0</title>
 
     <!-- Bootstrap Core CSS -->
     <link href="../vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
@@ -82,87 +34,105 @@ else if(isset($_POST['register'])){
     <!-- Custom Fonts -->
     <link href="../vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
 
+    <!-- CUstom Login Css -->
+     <link href="../dist/css/login.css" rel="stylesheet">
+     <link rel="shortcut icon" href="../img/favicon.ico" />
+
+
+    <!-- jQuery -->
+    <script src="../vendor/jquery/jquery.min.js"></script>
+
+     <!-- action.js for custom SCRIPT FOR AJAX request in LOGIN an REGISTER-->
+    <script src="../js/action.js"></script>
+
+
+
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     <!--[if lt IE 9]>
         <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
         <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
     <![endif]-->
-
 </head>
 
 <body>
+    <nav class="navbar navbar-inverse navbar-static-top" role="navigation">
+      <!-- Navbar content -->
+      <a class="navbar-brand" href="#">
+        <img src="../img/poso.png" width="120" height="120" class="d-inline-block align-top" alt="">
+      </a>
+        <span class="navbar-text">POSO TRAFFIC VIOLATION MANAGEMENT SYSTEM</span>
+    </nav>
+<br>
+<br>
+<div class="container">
+<div class="row">
+<div class="col-md-4 col-md-offset-4">
+<div class="form-body">
+    <ul class="nav nav-tabs final-login">
+        <li><a data-toggle="modal" data-target="#loginModal" href="#sectionA" onclick="_clearOnClick()">Sign In</a></li>
+        <li class="active"><a data-toggle="tab" href="#sectionB">New ACCOUNT</a></li>
+    </ul>
 
-    <div class="container">
-        <div class="row">
-            <div class="col-md-4 col-md-offset-4">
-                <div class="login-panel panel panel-default">
-                    <div class="panel-heading">
-                        <h3 class="panel-title">Please Sign In</h3>
-                    </div>
-                    <div class="panel-body">
-                        <form role="form" method="post" action="">
-                            <fieldset>
-                                <div class="form-group">
-                                    <input class="form-control" placeholder="Username" name="uname" type="username" autofocus>
-                                    <span class="error"><?php echo $err; ?></span>
-                                </div>
-                                <div class="form-group">
-                                    <input class="form-control" placeholder="Password" name="pass" type="password" value="">
-                                    <span class="error"><?php echo $errAllfields; ?></span>
-                                </div>
-                                <div class="checkbox">
-                                    <label>
-                                        <input name="remember" type="checkbox" value="Remember Me">Remember Me
-                                    </label>
-                                </div>
-                                <!-- Change this to a button or input when using this as a form -->
-                                <input type="submit" name="submitLogin" class="btn btn-lg btn-success btn-block" value="LOGIN">
-                            </fieldset>
-                        </form>
+    <!-- >>>>>>  LOGIN modal <<<< -->
+    <!-- >>>>>>>>>>>>>>>>>>>>>> -->
+    <div id="loginModal" class="modal fade" role="dialog">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h1>LOGIN YOUR<span class="badge badge-default">ACCOUNT</span></h1>
+                </div>
+                <div class="modal-body">
+                    <div class="tab-content">
+                        <div id="sectionA">
+                        <div class="innter-form">
+                            <form class="sa-innate-form" method="post" id="my-form">
+                            <label>Username</label>
+                            <input type="text" id="username" name="username" placeholder="Username">
+                            <label>Password</label>
+                            <input type="password" id="password" name="password" placeholder="Password">
+                            <input type="submit" id="login" name="submitLogin" value='Sign In' onclick="_loginScript()" />
+                            <a href="">Forgot Password?</a>
+                            </form>
+                            <div id="error" ></div>
+                            </div>
+                           
+                            <div class="clearfix"></div>
+                        </div>     
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-
-
-     <div class="container">
-        <div class="row">
-            <div class="col-md-4 col-md-offset-4">
-                <div class="login-panel panel panel-default">
-                    <div class="panel-heading">
-                        <h3 class="panel-title">Not Registered? Sign Up</h3>
-                    </div>
-                    <div class="panel-body">
-                        <form role="form" method="post" name="reg">
-                            <fieldset>
-                                <div class="form-group">
-                                    <input class="form-control" placeholder="Username" name="myuname" type="username" autofocus>
-                                </div>
-                                <div class="form-group">
-                                    <input class="form-control" placeholder="Password" name="mypass" type="password" value="">
-                                </div>
-                                 <div class="form-group">
-                                    <select name="mytype" class="form-control" placeholder="USER type" style="color: gray">
-                                        <option value="" disabled selected hidden>USER TYPE</option>
-                                        <option value="admin">Admin</option>
-                                        <option value="user">User</option>
-                                        <option value="super">Superuser</option>
-                                    </select>
-                                </div>
-                                <!-- Change this to a button or input when using this as a form -->
-                                <input id="register" name="register" class="btn btn-lg btn-success btn-block" value="REGISTER">
-                            </fieldset>
-                        </form>
-                    </div>
-                </div>
+    </div>  
+                 <!--- REGISTER-->
+        <div id="sectionB" class="tab-pane fade in active">
+            <div class="innter-form">
+            <form class="sa-innate-form" method="post" id="my_rform">
+            <label>Name</label>
+            <input type="text" name="r_username" id="r_username" placeholder="Your Username">
+            <label>Password</label>
+            <input type="password" name="r_password" id="r_password" placeholder="Your Password">
+            <label>User Type</label>
+                <select  id="r_type" name="r_type" placeholder="USER type" style="color: gray">
+                    <option value="" disabled selected hidden>USER TYPE</option>
+                    <option value="admin">Admin</option>
+                    <option value="user">User</option>
+                    <option value="superuser">Superuser</option>
+                </select>
+            <label>Enter Authorization Code</label>
+            <input type="password" name="r_auth" id="r_auth" placeholder="CONTACT JORDAN FOR AUTH CODE">
+            <input type="button" class="btn btn-danger btn-lg" value="Register" name="register" id="register" onclick="_registerUser()">
+            <p>By clicking Register, you agree to POSO's User Agreement, Privacy Policy, and Cookie Policy.</p>
+            </form>
+             <div id="r_error" style="color:red;font-style: italic; background: #efb96e; width: 100%" ></div>
             </div>
+              <div class="clearfix"></div>
         </div>
     </div>
-
-    <!-- jQuery -->
-    <script src="../vendor/jquery/jquery.min.js"></script>
+    </div>
+    </div>
+    </div>
+    </div>
 
     <!-- Bootstrap Core JavaScript -->
     <script src="../vendor/bootstrap/js/bootstrap.min.js"></script>
@@ -172,6 +142,9 @@ else if(isset($_POST['register'])){
 
     <!-- Custom Theme JavaScript -->
     <script src="../dist/js/sb-admin-2.js"></script>
+
+    <!-- Custom AJAX Login -->
+    <script src="../dist/js/login.js"></script>
 
 </body>
 
